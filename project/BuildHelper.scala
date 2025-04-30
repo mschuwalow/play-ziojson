f88e933303fe33b2509bebf12b336c153b98e324
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2025-04-30 01:48:51 +0000

    Update scala-library to 2.13.16

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -3,7 +3,7 @@ import Keys._
 import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object BuildHelper {
-  final val Scala213 = "2.13.15"
+  final val Scala213 = "2.13.16"
   final val Scala3   = "3.6.2"
 
   def extraCompilerOptions = Seq(
